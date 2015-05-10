d4a8336d6f605c46952cf0edc0a8815c7c230557
Mike Gerow <gerow@mgerow.com>
2015-05-10 13:26:45 -0700

    I think we might actually be initializing this thing right?

--- modules/ac97.c
+++ modules/ac97.c
@@ -11,11 +11,18 @@
 #include <pci.h>
 #include <system.h>
 
+/* utility functions */
+#define N_ELEMENTS(arr) (sizeof(arr) / sizeof((arr)[0]))
+
+/* BARs! */
 #define AC97_NAMBAR PCI_BAR0  /* Native Audio Mixer Base Address Register */
 #define AC97_NABMBAR PCI_BAR1  /* Native Audio Bus Mastering Base Address Register */
 
 /* Bus mastering offsets */
-#define AC97_PCM_OUT_BDL_BASE AC97_NAMBAR
+/* PCM out BAR */
+#define AC97_POBDBAR AC97_NABMBAR + 0x10
+/* PCM out last valid index */
+#define AC97_POLVI AC97_NABMBAR + 0x15
 
 /* Mixer settings */
 #define AC97_RESET             0x00
@@ -25,21 +32,21 @@
 #define AC97_PCM_OUT_VOLUME    0x18
 
 struct ac97_bdl_entry {
-	uint32_t pointer;
+	/* XXX(gerow): This relies on 32-bit addressing, probably bad */
+	uint8_t *pointer;
 	uint32_t control_and_length;
 } __attribute__((packed));
+
+#define AC97_CL_GET_LENGTH(cl) ((cl) & 0x00ff)
+#define AC97_CL_SET_LENGTH(cl, v) ((cl) = (v) & 0xff)
+#define AC97_CL_BUP 30
+#define AC97_CL_IOC 31
 
 struct ac97_device {
 	uint32_t pci_device;
 	int irq;
-	struct ac97_bdl_entry *buffer_descriptor_list[32];
-};
-
-struct buffer_descriptor_entry {
-	uint32_t ioc : 1;
-	uint32_t bup : 1;
-	uint32_t reserved : 14;
-	uint32_t buffer_length : 16;
+	/* Buffer descriptor list */
+	struct ac97_bdl_entry bdl[32];
 };
 
 static struct ac97_device _device;
@@ -78,13 +85,15 @@ static int init(void) {
 	outports(mixer_port + AC97_MASTER_VOLUME, 0);
 	outports(mixer_port + AC97_PCM_OUT_VOLUME, 0);
 
-	/* Allocate our buffer descriptor list */
-	_device.buffer_descriptor_list = malloc(sizeof(*_device.buffer_descriptor_list));
+	/* Give it our buffer descriptor list */
+	pci_write_field(_device.pci_device, AC97_POBDBAR, 4, (uint32_t) &_device.bdl);
+	pci_write_field(_device.pci_device, AC97_POLVI, 1, N_ELEMENTS(_device.bdl));
+
 	return 0;
 }
 
 static int fini(void) {
-	free(_device.buffer_descriptor_list);
+	free(_device.bdl);
 	return 0;
 }
 
