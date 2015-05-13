c7391234c29e8cc3f5347755018c2ca4f609e117
Mike Gerow <gerow@mgerow.com>
2015-05-13 01:26:29 -0700

    backed out debug shell change

--- modules/debug_shell.c
+++ modules/debug_shell.c
@@ -271,6 +271,14 @@ static void scan_hit_list(uint32_t device, uint16_t vendorid, uint16_t deviceid,
 			deviceid,
 			pci_vendor_lookup(vendorid),
 			pci_device_lookup(vendorid,deviceid));
+
+	fprintf(tty, " BAR0: 0x%8x\n", pci_read_field(device, PCI_BAR0, 4));
+	fprintf(tty, " BAR1: 0x%8x\n", pci_read_field(device, PCI_BAR1, 4));
+	fprintf(tty, " BAR2: 0x%8x\n", pci_read_field(device, PCI_BAR2, 4));
+	fprintf(tty, " BAR3: 0x%8x\n", pci_read_field(device, PCI_BAR3, 4));
+	fprintf(tty, " BAR4: 0x%8x\n", pci_read_field(device, PCI_BAR4, 4));
+	fprintf(tty, " BAR6: 0x%8x\n", pci_read_field(device, PCI_BAR5, 4));
+
 }
 
 static int shell_pci(fs_node_t * tty, int argc, char * argv[]) {
